b07c63951740904968a17526a0096eee3611da11
John <laojohnmatthew@gmail.com>
2020-02-10 12:32:22 -0800

    finish everything

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,23 @@
+// var Bee = function() {
+//   Grub.call(this);
+// };
+// Bee.prototype = Object.create(Grub.prototype);
+// Bee.prototype.contructor = Bee;
+// Bee.prototype.color = 'yellow';
+// Bee.prototype.age = 5;
+// Bee.prototype.job = 'keep on growing';
+
+// var bee = new Bee();
+// console.log(bee)
+
+
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
